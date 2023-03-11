986e55aaaeee178b8aacb3227d3a33821c9e7ecd
Dinesh <dineshkumarpaliwal83@gmail.com>
2023-03-12 02:09:33 +0530

    user interface created

--- src/app.js
+++ src/app.js
@@ -3,14 +3,19 @@ const app=express()
 const path=require("path")                                     //for fetching path detail
 const session=require("express-session");
 
+
+
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))  //session middleware
 
 
 require("./db/connect")                                        //mongodb connection
-const collection =require("./model/logindetail")
+const collection =require("./model/logindetail")               //user login detail
+
+const LendItem =require("./model/lending")            //lendingstore 
+
 
 app.use(express.json())                                        //as a bodyparser
-app.use(express.urlencoded({extended:false}))                  
+app.use(express.urlencoded({extended:true}))                  
 
 const port=process.env.PORT || 3000
 const ejs=require("ejs")                                       //for register partial method
@@ -53,10 +58,12 @@ app.post("/signup",async(req,res)=>{
             email:req.body.email,
             password:req.body.password,
             phone:req.body.phone
+            
         }
         await collection.insertMany([data])
-
-        res.render("home2",{user:req.body})
+        
+        res.render("login");
+ 
     })
     
 
@@ -92,16 +99,90 @@ app.post("/login",async(req,res)=>{
     }
 })
 
+
+
 app.get("/home2",(req,res)=>{
-   
+        
         res.render("home2");
 
 })
 
+app.get("/profile/:id",async(req,res)=>{
+    try{
+        const userData=await collection.findById({_id:req.params.id})
+        res.render("profile",{user:userData});
+    }
+    catch(err){
+        res.render(err);
+    }
+})
 
+// ..............................
+// landing page
+app.get("/profile/:id/lend",async(req,res)=>{
+    try{
+        const userData=await collection.findById({_id:req.params.id})
+        res.render("lend",{user:userData});
+    }
+    catch(err){
+        res.render(err);
+    }
    
+})
+
+
+
+
+
+app.post("/profile/:id/lend",async(req,res)=>{
     
+        const data={
+            name:req.body.name,
+            price:req.body.price,
+            time:req.body.time,
+            sellername:req.body.username,
+            sellerphone:req.body.userphone,
+            sellerid:req.body.userid
+        }
+        await LendItem.insertMany([data])
+        res.render("home")
+  
+})
+
+app.get("/:id/userstore",async(req,res)=>{
    
+     try{
+        const check=await LendItem.find({sellerid:req.params.id});
+        res.render("userstore",{record:check});
+     }
+  catch{
+    console.log("not")
+  }
+})
+
+
+app.get("/store",async(req,res)=>{
+    try{
+        const check=await LendItem.find({});
+        res.render("store",{record:check});
+     }
+  catch{
+    console.log("not")
+  }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -109,3 +190,4 @@ app.get("/home2",(req,res)=>{
 app.listen(port,(req,res)=>{
     console.log("server is up")
 })
+
